c5ef1139efda2889f8567575aae00371d0ef91f2
NikolVasileva <nikol.vasileva9@gmail.com>
2026-02-14 17:18:29 +0200

    update status bar style

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ export default function App() {
   return (
     <SafeAreaProvider>
         <NavigationContainer>
-          <StatusBar style="auto" />
+          <StatusBar translucent backgroundColor="transparent" style="dark"/>
           <RootNavigator />
         </NavigationContainer>
     </SafeAreaProvider>
