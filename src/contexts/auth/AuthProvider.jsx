d62342494029b2c8e7f7e68fc12940400db22b50
NikolVasileva <nikol.vasileva9@gmail.com>
2026-02-15 16:23:38 +0200

    create user to context values and auth service file

--- src/contexts/auth/AuthProvider.jsx
+++ src/contexts/auth/AuthProvider.jsx
@@ -1,10 +1,14 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
+
+export const AuthContext = createContext();
 
 export default function AuthProvider({ children }) {
 
-    const AuthContext = createContext();
-    
-    const contextValue = {}
+    const [user, setUser] = useState(null)
+
+    const contextValue = {
+        user,
+    }
 
     return(
         <AuthContext.Provider value={contextValue}>
