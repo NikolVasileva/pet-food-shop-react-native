32b481e44c54e9694d52249316eaa95d17a95a66
NikolVasileva <nikol.vasileva9@gmail.com>
2026-02-28 18:49:22 +0200

    create banner container on offer screen

--- src/screens/OfferScreen.jsx
+++ src/screens/OfferScreen.jsx
@@ -1,11 +1,33 @@
+import { Image, StyleSheet } from "react-native";
 import { ScrollView, Text, View } from "react-native";
 
 export default function OfferScreen() {
-    return(
+    return (
         <ScrollView>
             <View>
-                <Text>Test</Text>
+                <View style={styles.bannerContainer}>
+                    <Image source={require("../../assets/offer-banner.png")}
+                        style={styles.bannerImage} />
+                </View>
             </View>
         </ScrollView>
     )
 }
+
+const styles = StyleSheet.create({
+    bannerContainer: {
+        borderBottomRightRadius: 100,
+        borderBottomLeftRadius: 100,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.1,
+        shadowRadius: 4,
+        elevation: 3,
+    },
+    bannerImage: {
+        width: "100%",
+        height: 250,
+        borderBottomRightRadius: 100,
+        borderBottomLeftRadius: 100, 
+    }
+})
