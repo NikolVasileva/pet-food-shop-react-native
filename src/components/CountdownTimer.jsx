870627e44b83dfd6bf0025e94e45e82aa1b3587b
NikolVasileva <nikol.vasileva9@gmail.com>
2026-02-28 19:52:30 +0200

    Update countdown timer visualization

--- src/components/CountdownTimer.jsx
+++ src/components/CountdownTimer.jsx
@@ -1,14 +1,14 @@
 import { useEffect, useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 
-export default function CountdownTimer({endDate}) {
+export default function CountdownTimer({ endDate }) {
     // const [time, setTime] = useState(Date.now());
     // const [endTime, setEndTime] = useState(endDate);
 
     const getTimeLeft = () => {
         const diff = new Date(endDate).getTime() - Date.now();
 
-        if (diff <= 0){
+        if (diff <= 0) {
             return null
         }
 
@@ -42,33 +42,58 @@ export default function CountdownTimer({endDate}) {
 
     return (
         <View style={styles.container}>
-          {timeLeft.days > 0 && (
-            <>
-              <Text style={styles.time}>{timeLeft.days} дни : </Text>
-            </>
-          )}
-          <Text style={styles.time}>{pad(timeLeft.hours)} ч. : </Text>
-          <Text style={styles.time}>{pad(timeLeft.minutes)} мин. : </Text>
-          <Text style={styles.time}>{pad(timeLeft.seconds)} сек. </Text>
+            {timeLeft.days > 0 && (
+                <View style={styles.elements}>
+                    <Text style={[styles.circleText, styles.circle]}>{timeLeft.days}</Text>
+                    <Text style={styles.time}>days</Text>
+                </View>
+            )}
+            <View style={styles.elements}>
+                <Text style={[styles.circleText, styles.circle]}>{pad(timeLeft.hours)}</Text>
+                <Text style={styles.time}>hours</Text>
+            </View>
+            <View style={styles.elements}>
+                <Text style={[styles.circleText, styles.circle]}>{pad(timeLeft.minutes)}</Text>
+                <Text style={styles.time}>min</Text>
+            </View>
+            <View style={styles.elements}>
+                <Text style={[styles.circleText, styles.circle]}>{pad(timeLeft.seconds)}</Text>
+                <Text style={styles.time}>sec</Text>
+            </View>
         </View>
-      );
-    
+    );
+
 }
 
 const styles = StyleSheet.create({
     container: {
-      flexDirection: "row",
-      alignItems: "center",
-      justifyContent: "center",
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "center",
+        gap: 20
     },
     time: {
-      fontSize: 18,
-      fontWeight: "600",
-      color: "#925076",
+        fontSize: 18,
+        fontWeight: "600",
+        color: "#000",
     },
     expired: {
-      fontSize: 14,
-      color: "#999",
+        fontSize: 14,
+        color: "#999",
     },
-  });
-  
+    elements: {
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    circle: {
+        backgroundColor: "#00B8BD",
+        borderRadius: "50%",
+        padding: 12,
+    },
+    circleText: {
+        fontSize: 18,
+        fontWeight: "600",
+        color: "#fff",
+    }
+});
