1d17efb787c09e6f89bcd263ab20157aff752324
NikolVasileva <nikol.vasileva9@gmail.com>
2026-03-01 18:54:31 +0200

    Add cart screen to the tab menu

--- src/navigator/ShopNavigator.jsx
+++ src/navigator/ShopNavigator.jsx
@@ -1,18 +1,33 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { FontAwesome } from "@expo/vector-icons";
+import { FontAwesome, Ionicons } from "@expo/vector-icons";
 import HomeNavigator from "./HomeNavigator";
+import CartScreen from "../screens/CartScreen";
 
 export default function ShopNavigator() {
     const Tabs = createBottomTabNavigator();
 
     return (
-        <Tabs.Navigator>
+        <Tabs.Navigator screenOptions={{
+            tabBarActiveTintColor: "#000",
+            tabBarInactiveTintColor: "#000",
+          }}>
             <Tabs.Screen
                 name="Home"
                 component={HomeNavigator}
                 options={{
                     headerShown: false, tabBarIcon: ({ focused, size }) => (
-                        <FontAwesome name={focused ? "heart" : "heart-o"} size={size} />
+                        <Ionicons name={focused ? "home-sharp" : "home-outline"} size={size}
+                        color={focused ? "#00B8BD" : "#000"}  />
+                    ),
+                }}
+            />
+            <Tabs.Screen
+                name="Cart"
+                component={CartScreen}
+                options={{
+                    headerShown: false, tabBarIcon: ({ focused, size }) => (
+                        <Ionicons name={focused ? "cart-sharp" : "cart-outline"} size={size}
+                        color={focused ? "#00B8BD" : "#000"}  />
                     ),
                 }}
             />
